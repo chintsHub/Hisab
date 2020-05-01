706cab79d1a26161890ec11b81fe3836648e426a
chintan <cshah@assetic.com>
2020-05-01 21:49:02 +1000

    Ability to update Password
    Ability to update Password

--- Hisab.UI/Pages/App/UserSettings.cshtml.cs
+++ Hisab.UI/Pages/App/UserSettings.cshtml.cs
@@ -15,20 +15,30 @@ namespace Hisab.UI
 {
     public class UserSettingsModel : PageModel
     {
-        [BindProperty(SupportsGet =true)]
+        [BindProperty(SupportsGet = true)]
         public UserSettingsVm UserSettingsVm { get; set; }
 
+
+        [BindProperty]
+        public ChangePasswordVm PasswordVm { get; set; }
 
         public string UserSettingsMessage { get; set; }
 
         private IUserSettingManager _userSettingManager;
         private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
 
-        public UserSettingsModel(IUserSettingManager UserSettingManager, UserManager<ApplicationUser> userManager)
+        public UserSettingsModel(IUserSettingManager UserSettingManager,
+                UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager)
         {
             _userSettingManager = UserSettingManager;
-            UserSettingsVm = new UserSettingsVm();
+
             _userManager = userManager;
+            _signInManager = signInManager;
+
+            UserSettingsVm = new UserSettingsVm();
+            PasswordVm = new ChangePasswordVm();
         }
         public async Task<IActionResult> OnGet()
         {
@@ -46,14 +56,14 @@ namespace Hisab.UI
 
         public async Task<IActionResult> OnPostUserSettings()
         {
-            
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
 
                 var user = await _userManager.FindByNameAsync(User.Identity.Name);
                 var result = await _userSettingManager.UpdateUserSettings(UserSettingsVm.UserName, user.Id, (AvatarEnum)UserSettingsVm.SelectedAvatarId);
 
-                if(result)
+                if (result)
                 {
                     UserSettingsMessage = "Settings updated sucessfully";
                 }
@@ -63,9 +73,46 @@ namespace Hisab.UI
                 }
 
             }
-            
+
 
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostUpdatePassword()
+        {
+            if (!String.IsNullOrEmpty(PasswordVm.NewPassword))
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+
+                var result = await _userManager.ResetPasswordAsync(user, token, PasswordVm.NewPassword);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignOutAsync();
+                    return RedirectToPage("/LoginPage");
+                }
+                else
+                {
+                    AddErrors(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "You must provide a password");
+            }
+
+
+            return Page();
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            }
         }
     }
 }
